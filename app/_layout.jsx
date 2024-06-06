9c7e8256d005e915cb15798df52634680511d3df
seewruuk <sewruczekk@icloud.com>
2024-06-06 19:13:47 +0200

    first commit from webstorm

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const RootLayout = () => {
+    return(
+        <View className={"bg-white"}>
+            <Text>RootLayout</Text>
+        </View>
+    )
+}
+
+export default RootLayout;
---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const RootLayout = () => {
+    return(
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+
+export default RootLayout;
